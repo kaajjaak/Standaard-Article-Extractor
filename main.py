c61369bc081fd3d9a0d754b26c60794dd089d2ef
kaajjaak <yolobroer202@gmail.com>
2023-10-19 17:02:42 +0200

    Fixed error where it wouldn't format the file properly

--- main.py
+++ main.py
@@ -13,5 +13,20 @@ print(article)
 if os.path.exists("article.html"):
     os.remove("article.html")
 file = open("article.html", "w", encoding="utf-8")
+file.write("""
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+""")
 file.write('<link rel="stylesheet" href="https://www.standaard.be/v2/article-detail/cdn/fragment-article-detail.0.0.792.ds.styles.css">')
 file.write(article)
+file.write("""
+</body>
+</html>""")
